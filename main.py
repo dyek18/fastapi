cc811d7342fa1d30dc1eb5fffc2e99c461cc1a3b
dyek18 <gjxodn000@naver.com>
2023-10-12 13:41:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="public", html = True), name="static")
+app.mount("/", StaticFiles(directory="public", html=True), name="static")
+
+@app.get("/")
+def read_root():
+    custom_message = "내가 하고 싶은 말을 여기에 추가하세요."
+    return HTMLResponse(content=f"<html><body><h1>Hello, World</h1><p>{custom_message}</p></body></html>")
